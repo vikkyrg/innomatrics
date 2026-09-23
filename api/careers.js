0d96361d49f1655954bf7873155de5d5c61ce005
vikkyrg <rvikky05@gmail.com>
2026-09-23 11:52:58 +0530

    careers API

--- api/careers.js
+++ api/careers.js
@@ -30,15 +30,6 @@ const escapeHtml = (value = "") =>
     .replace(/'/g, "&#039;");
 
 module.exports = async (req, res) => {
-  // --- CORS (the static site calls this function cross-origin on previews) ---
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.status(204).end();
-  }
-
   if (req.method !== "POST") {
     return res.status(405).json({ success: false, message: "Method not allowed." });
   }
